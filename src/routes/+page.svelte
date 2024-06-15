3f40f4c82fc054d6b582bce4675c5c571d2bc768
Tarpstone <mike@tarpey.io>
2024-06-15 00:03:51 -0400

    Overwhelmingly rough concept

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+    <header>
+        <h1>2024</h1>
+    </header>
+    <section>
+        <header>
+            <h2>dreamhack dallas</h2>
+        </header>
+        <div class="album-grid">
+            <enhanced:img
+                class="album-grid-img"
+                src="$lib/images/albums/2024-dreamhack-dallas/baseball.webp"
+                alt="From left to right sitting in the stands at Truist Park in Atlanta, GA: Brandon, Mike Tarpey, Bryant."
+            />
+            <enhanced:img
+                class="album-grid-img"
+                src="$lib/images/albums/2024-dreamhack-dallas/ddr.webp"
+                alt="Pictured on the left is James Banks, the premier player interviewer and stage hype-man in Counter-Strike esports. Pictured on the right is Mike Tarpey, owner of this website (tarpey.dev)."
+            />
+            <enhanced:img
+                class="album-grid-img"
+                src="$lib/images/albums/2024-dreamhack-dallas/banks.webp"
+                alt="Pictured on the left is James Banks, the premier player interviewer and stage hype-man in Counter-Strike esports. Pictured on the right is Mike Tarpey, owner of this website (tarpey.dev)."
+            />
+            <enhanced:img
+                class="album-grid-img"
+                src="$lib/images/albums/2024-dreamhack-dallas/kaelaris.webp"
+                alt="Pictured on the left is James Banks, the premier player interviewer and stage hype-man in Counter-Strike esports. Pictured on the right is Mike Tarpey, owner of this website (tarpey.dev)."
+            />
+            <enhanced:img
+                class="album-grid-img"
+                src="$lib/images/albums/2024-dreamhack-dallas/gstew.webp"
+                alt="Pictured on the left is James Banks, the premier player interviewer and stage hype-man in Counter-Strike esports. Pictured on the right is Mike Tarpey, owner of this website (tarpey.dev)."
+            />
+        </div>
+    </section>
+
+</main>
+
+<style>
+    main {
+        max-width: 800px;
+    }
+
+    .album-grid {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .album-grid-img {
+        height: 200px;
+        width: 200px;
+        object-fit: cover;
+    }
+</style>
