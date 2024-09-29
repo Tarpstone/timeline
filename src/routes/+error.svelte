cb6c9afe61a018d764733e063890227ce3845180
Tarpstone <mike@tarpey.io>
2024-09-28 23:56:19 -0400

    Add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+	import AboutFooter from '$lib/components/AboutFooter.svelte';
+	import Seo from '$lib/components/Seo.svelte';
+</script>
+
+<Seo />
+<main>
+    <h1>{$page.status} {$page.error?.message ?? "Unknown error..."}</h1>
+</main>
+<AboutFooter />
--- src/routes/[year]/+page.ts
+++ src/routes/[year]/+page.ts
@@ -1,8 +1,12 @@
 import { getYearKeyImages, type EnhancedImageData } from '$lib/db';
+import { error } from '@sveltejs/kit';
 
 // load is a special function in Svelte. The return value is made available
 // to the neighboring page.svelte file
 export function load({ params }): { year: string; images: EnhancedImageData[] } {
 	const imageData = getYearKeyImages(params.year);
+	if (!imageData) {
+		error(404, "This page doesn't exist.");
+	}
 	return { year: params.year, images: imageData };
 }
