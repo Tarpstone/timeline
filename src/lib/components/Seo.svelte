5f07ad80f8590283c9cd522aa2106353ba167725
Tarpstone <mike@tarpey.io>
2025-03-11 23:47:23 -0400

    Relax requirements for SEO image

--- src/lib/components/Seo.svelte
+++ src/lib/components/Seo.svelte
@@ -29,7 +29,7 @@
 		fullPageUrl = `${basePageUrl}/${urlOverride}`;
 	}
 	// fallback to default SEO image if something is messed up
-	const alternateSEOImageReady = imageUrl && imageAltText;
+	const alternateSEOImageReady = imageUrl; // && imageAltText;
 	const finalImageUrl = `${alternateSEOImageReady ? imageUrl : getImageBuildPaths('1992', 'life', 'van').src1600}`;
 	const defaultAltText =
 		'Cindy Tarpey in the early 90s, crouched down on the front lawn of her house in Central Florida, holding her toddler aged son Mike Tarpey (owner of this website). The shadows indicate high noon, and their blocky brown Chrysler minivan is in the background.';
