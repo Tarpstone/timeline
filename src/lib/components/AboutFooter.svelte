8368b95fc70b3682ee363a8edd3d7512823da19f
Tarpstone <mike@tarpey.io>
2025-05-14 16:54:18 -0400

    Colby's suggested fix for back button to preserve page location

--- src/lib/components/AboutFooter.svelte
+++ src/lib/components/AboutFooter.svelte
@@ -5,7 +5,7 @@
 <footer>
 	{#if !home}
 		<!-- go up a level -->
-		<a href=".">←back</a>
+		<button id="back" on:click={() => history.back()}>←back</button>
 	{:else}
 		<a href="/about">about→</a>
 	{/if}
@@ -15,5 +15,15 @@
 	a {
 		font-size: 1.5rem;
 		font-weight: 700;
+	}
+
+	#back {
+		color: white;
+		background-color: black;
+		border: none;
+		font-family: 'Lekton', monospace;
+		font-size: 1.5rem;
+		font-weight: 700;
+		cursor: pointer;
 	}
 </style>
